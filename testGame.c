1ece610c79bfac3e5aecd85461c98b062487f58f
Raieven <darkdrakkonsun41@gmail.com>
2016-05-05 11:27:49 +1000

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -3,9 +3,45 @@
 
 #include "Game.h"
 
+#define DEFAULT_DISCIPLINES {STUDENT_BQN, STUDENT_MMONEY, STUDENT_MJ, \
+                STUDENT_MMONEY, STUDENT_MJ, STUDENT_BPS, STUDENT_MTV, \
+                STUDENT_MTV, STUDENT_BPS,STUDENT_MTV, STUDENT_BQN, \
+                STUDENT_MJ, STUDENT_BQN, STUDENT_THD, STUDENT_MJ, \
+                STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS}
+                
+#define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
+
 int main (int argc, char* argv[]){
 
+    int disciplines[] = DEFAULT_DISCIPLINES;
+    int dice[] = DEFAULT_DICE;
+    Game g = newGame (disciplines, dice);
 
+}
 
-
+void testGame(){
+    
+    int i = getTurnNumber (g);
+    throwDice(g);
+    assert(getTurnNumber(g) == i+1);
+    
+    getTurnNumber(g) = -1;
+    assert(getKPIpoints(g, 1) == 24);
+    assert(getKPIpoints(g, 2) == 24);
+    assert(getKPIpoints(g, 3) == 24);
+    
+    getARCs(g, 1);
+    assert(getKPIpoints(g, 1) == 26);
+    
+    getG08s(g, 1);
+    assert(getKPIpoints(g, 1) == 46);
+    
+    getCampuses(g, 1);
+    assert(getKPIpoints(g, 1) == 56);
+    
+    getIPs (g, 1);
+    assert(getKPIpoints(g, 1) == 66);
+    
+    getPublications(g, 1);
+    assert(getKPIpoints(g, 1) == 66);
 }
