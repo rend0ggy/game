aef48c741082e7ffbddbd7be9553a4b91f44f481
Raieven <darkdrakkonsun41@gmail.com>
2016-05-09 11:04:47 +1000

    Update Game.h
    uncommented default games (so itll compile)

--- Game.h
+++ Game.h
@@ -123,14 +123,14 @@ typedef struct _action {
 //
 // so to create the default game as shown on the badly drawn map:
 //
-/*
+
 #define DEFAULT_DISCIPLINES {STUDENT_BQN, STUDENT_MMONEY, STUDENT_MJ, \
                 STUDENT_MMONEY, STUDENT_MJ, STUDENT_BPS, STUDENT_MTV, \
                 STUDENT_MTV, STUDENT_BPS,STUDENT_MTV, STUDENT_BQN, \
                 STUDENT_MJ, STUDENT_BQN, STUDENT_THD, STUDENT_MJ, \
                 STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS}
 #define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
-*/
+
 //
 //   int disciplines[] = DEFAULT_DISCIPLINES;
 //   int dice[] = DEFAULT_DICE;
