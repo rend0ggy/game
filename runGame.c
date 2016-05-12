1de7d745309b6f9a50ebf377e96a7b9dde20a99d
rend0ggy <jack.renshaw97@gmail.com>
2016-05-12 11:28:06 +1000

    Update runGame.c

--- runGame.c
+++ runGame.c
@@ -1,1 +1,18 @@
+#include "Game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(void)
+{
+  Game game = newGame();
+  int gameAlive = TRUE;
+  while(gameAlive == TRUE)
+  {
+    int diceValue = diceThrow();
+    diceValue += diceThrow();
+    rollDice(game,diceValue);
+    int whoseTurn = getWhoseTurn(game);
+    printf("It is player %d's turn\n",whoseTurn);
+  }
+};
