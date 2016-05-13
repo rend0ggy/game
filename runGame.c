59fcd721bd2b593b089c9ed00a583cf07fb0b529
Raieven <darkdrakkonsun41@gmail.com>
2016-05-13 19:22:41 +1000

    Update runGame.c

--- runGame.c
+++ runGame.c
@@ -3,16 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void)
-{
-  Game game = newGame();
-  int gameAlive = TRUE;
-  while(gameAlive == TRUE)
-  {
-    int diceValue = diceThrow();
-    diceValue += diceThrow();
-    rollDice(game,diceValue);
-    int whoseTurn = getWhoseTurn(game);
-    printf("It is player %d's turn\n",whoseTurn);
-  }
-};
+int main (int argc, char * argv[]){
+    Game game = newGame();
+    int gameAlive = TRUE;
+    while(gameAlive == TRUE){
+        int diceValue = diceThrow();
+        diceValue += diceThrow();
+        rollDice (game, diceValue);
+        
+        int whoseTurn = getWhoseTurn(game);
+        printf("It is player %d's turn\n", whoseTurn);
+        
+        // player chooses action
+        // while loop:
+        // save action in actionCode
+        // check if legal
+        // change game state
+        // 
+        // following while loop not working yet --> fixing Game.c (finishing/fixing functions)
+        while (playerturn notover){
+            int action = 0;
+            printf("Choose an action:\n");
+            scanf("%d", &action);
+            player->a.actionCode = action;
+            makeAction(game, action);
+        }
+    }
+    
+    return EXIT_SUCCESS;
+}
